de1676f9f10d040fb3fbd6e60f28eb2a2a38d939
mi <997326986@qq.com>
2020-11-19 16:15:58 +0800

    第一次提交

---
+++ src/main/java/com/fh/controller/TestController.java
@@ -0,0 +1,15 @@
+package com.fh.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("test")
+@RestController
+public class TestController {
+
+    @RequestMapping("test")
+    private String test(){
+        return "1111";
+    }
+
+}
